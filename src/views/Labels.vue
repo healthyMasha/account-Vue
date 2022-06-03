69ee8ddeec3c668bef6a21c19545ccc95183c26e
withoutiger <344195937@qq.com>
2022-06-04 00:21:25 +0800

    add new file

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Layout>
+        label.vue
+        </Layout>
+    </div>
+</template>
+
+<script lang="ts">
+import Layout from "@/components/Layout.vue";
+    export default {
+    name: "Labels",
+    components: { Layout }
+};
+</script>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Layout>
+        statistics.vue
+        </Layout>
+    </div>
+</template>
+
+<script lang="ts">
+import Layout from "@/components/Layout.vue";
+    export default {
+    name: "Statistics",
+    components: { Layout }
+};
+</script>
